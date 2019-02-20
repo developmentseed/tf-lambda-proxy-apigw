1d2bcead2ca837fb4c3626c5b4c4b548cfee298f
vincentsarago <vincent.sarago@gmail.com>
2019-02-20 15:42:36 -0500

    typos 2

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,23 +1,23 @@
-output "lamda_fuction_name" {
+output "lambda_fuction_name" {
   value = "${aws_lambda_function.lambda.function_name}"
 }
 
-output "lamda_fuction_id" {
+output "lambda_fuction_id" {
   value = "${aws_lambda_function.lambda.id}"
 }
 
-output "lamda_fuction_arn" {
+output "lambda_fuction_arn" {
   value = "${aws_lambda_function.lambda.arn}"
 }
 
-output "lamda_fuction_arn_invoke" {
+output "lambda_fuction_arn_invoke" {
   value = "${aws_lambda_function.lambda.invoke_arn}"
 }
 
-output "lamda_role_name" {
+output "lambda_role_name" {
   value = "${aws_iam_role.lambda_role.name}"
 }
 
-output "lamda_role_id" {
+output "lambda_role_id" {
   value = "${aws_iam_role.lambda_role.id}"
 }
